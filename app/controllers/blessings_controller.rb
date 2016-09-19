74cce745cd0fce65e058c51791d615b3d9a9364e
pinglamb <pinglambs@gmail.com>
2016-09-20 06:38:21 +0800

    Happy claim button

---
+++ app/controllers/blessings_controller.rb
@@ -0,0 +1,4 @@
+class BlessingsController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  resources :blessings
   resources :insurances do
     member do
       get :pay
+      get :claim
     end
   end
 
   devise_for :users
-  root to: 'home#index'
+  root to: 'insurances#index'
 end
